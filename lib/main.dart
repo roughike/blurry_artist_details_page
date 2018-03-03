970279f18be076792e90522d62bb3555fd1d16a5
Iiro Krankka <iiro.krankka@codemate.com>
2018-03-03 14:21:45 +0200

    Added two new videos and reordered the thumbs.

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,18 @@ class MyApp extends StatelessWidget {
         url: 'https://www.youtube.com/watch?v=_FhCilozomo',
       ),
       new Video(
-        title: 'Free - I\'ll Be Creepin\' - Live at Granada Studios 1970',
+        title: 'Free - Ride on a Pony - Live at Granada Studios 1970',
         thumbnail: 'assets/video2_thumb.png',
+        url: 'https://www.youtube.com/watch?v=EDHNZuAnBoU',
+      ),
+      new Video(
+        title: 'Free - Songs of Yesterday - Live at Granada Studios 1970',
+        thumbnail: 'assets/video3_thumb.png',
+        url: 'https://www.youtube.com/watch?v=eI1FT0a_bos',
+      ),
+      new Video(
+        title: 'Free - I\'ll Be Creepin\' - Live at Granada Studios 1970',
+        thumbnail: 'assets/video4_thumb.png',
         url: 'https://www.youtube.com/watch?v=3qK8O3UoqN8',
       ),
     ],
